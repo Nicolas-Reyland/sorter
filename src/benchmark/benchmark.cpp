936a6a2de88671efb8adc30d5ce3aee01a41c2e5
Nicolas Reyland <nicolas@reyland.dev>
2023-03-11 22:35:09 +0100

    stdout: added progress bar

--- src/benchmark/benchmark.cpp
+++ src/benchmark/benchmark.cpp
@@ -4,6 +4,11 @@
 
 #include "stage.hpp"
 
+#define PBSTR "============================================================"
+#define PBWIDTH 60
+
+static void print_progress(double percentage);
+
 void benchmark(const struct BenchSettings& settings)
 {
     size_t num_stages, size_increment;
@@ -20,15 +25,40 @@ void benchmark(const struct BenchSettings& settings)
     }
     init_benchmark_output(settings);
     size_t arr_size = MIN_ARR_SIZE;
+    // arithmetic series sum formula
+    size_t total_processed_size =
+        num_stages * ((arr_size + settings.arr_size_limit) / 2);
+    size_t curr_processed_size = 0;
+    // progress bar
+    int percentage = 0;
+    int last_percentage = -1;
+
     for (size_t stage_index = 0; stage_index < num_stages; ++stage_index)
     {
-        std::cout << "Starting stage " << stage_index + 1 << "/" << num_stages
-                  << std::endl;
         // run the stage
         run_stage(settings, arr_size);
+        // progress bar
+        if (percentage - last_percentage >= 1)
+        {
+            print_progress((double)percentage / 100);
+            last_percentage = percentage;
+        }
+        curr_processed_size += arr_size;
+        percentage =
+            (int)(((size_t)100 * curr_processed_size) / total_processed_size);
         // prepare the next stage
         arr_size += size_increment;
     }
-    std::cout << "Done running all the " << num_stages << " stages"
-              << std::endl;
+    print_progress(1.0);
+    std::cout << std::endl
+              << "Done running " << num_stages << " stages" << std::endl;
+}
+
+static void print_progress(double percentage)
+{
+    int val = (int)(percentage * 100);
+    int lpad = (int)(percentage * PBWIDTH);
+    int rpad = PBWIDTH - lpad;
+    printf("\r%3d%% [%.*s%*s]", val, lpad, PBSTR, rpad, "");
+    fflush(stdout);
 }
