20ded298e9d4773bbb711e1629dc73dea474f093
Nicolas Reyland <nicolas@reyland.dev>
2023-03-19 00:19:03 +0100

    feat: smoothen the curve using -s/--smooth option

--- visualize.py
+++ visualize.py
@@ -5,11 +5,18 @@ import csv
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
+from argparse import ArgumentParser
 
 matplotlib.use("tkagg")
 
 
-def main(path: str) -> int:
+def smooth(y, box_pts):
+    box = np.ones(box_pts) / box_pts
+    y_smooth = np.convolve(y, box, mode="same")
+    return y_smooth
+
+
+def main(path: str, smooth_val: int = 0) -> int:
     if not os.path.isfile(path):
         print(f'Cannot find "{path}"', file=sys.stderr)
         return 1
@@ -21,6 +28,10 @@ def main(path: str) -> int:
         )
     size_arr = data[:, 0]
     times_matrix = data[:, 1:].T
+    if smooth_val > 0:
+        times_matrix = np.apply_along_axis(
+            lambda y: smooth(y, smooth_val), 0, times_matrix
+        )
     for name, times in zip(names, times_matrix):
         plt.plot(size_arr, times, label=name)
     plt.xlabel("array size")
@@ -31,5 +42,11 @@ def main(path: str) -> int:
 
 
 if __name__ == "__main__":
-    exit_code: int = main(sys.argv[1] if len(sys.argv) > 1 else "output.csv")
+    parser = ArgumentParser(
+        description="Sorting algorithms benchmarking results visualization"
+    )
+    parser.add_argument("file", nargs="?", default="output.csv")
+    parser.add_argument("-s", "--smooth", type=int, default=0)
+    args = parser.parse_args()
+    exit_code: int = main(args.file, args.smooth)
     sys.exit(exit_code)
